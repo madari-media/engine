22d7db0c412f20d3e2c4ba84b14e6b871746ede4
omkar <omkar@anthaathi.org>
2025-02-15 17:07:53 +0530

    expose type

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
 ## 1.0.0
 
 - Initial version.
+
+
+## 1.0.1
+
+- Exposed the types for the stremio
--- lib/madari_engine.dart
+++ lib/madari_engine.dart
@@ -1,3 +1,4 @@
 library;
 
+export './src/models/stremio_addons_types.dart';
 export 'src/madari_engine_base.dart';
